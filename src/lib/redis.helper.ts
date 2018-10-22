4f8d7f7dafd4e5dea91f590ace1c972af2903525
watsonzuo <watsonzuo@httpeace.com>
2018-10-22 13:15:46 +0800

    #add console message in redis helper

--- src/lib/redis.helper.ts
+++ src/lib/redis.helper.ts
@@ -1,3 +1,29 @@
 import { createClient } from 'redis';
 
-export const RedisHelper = createClient();
+export const RedisHelper = createClient({
+  retry_strategy: function(options) {
+    if (options.error && options.error.code === 'ECONNREFUSED') {
+      // End reconnecting on a specific error and flush all commands with
+      // a individual error
+      return new Error('The server refused the connection');
+    }
+    if (options.total_retry_time > 1000 * 60 * 60) {
+      // End reconnecting after a specific timeout and flush all commands
+      // with a individual error
+      return new Error('Retry time exhausted');
+    }
+    if (options.attempt > 10) {
+      // End reconnecting with built in error
+      return undefined;
+    }
+    // reconnect after
+    return Math.min(options.attempt * 100, 3000);
+  }
+});
+
+RedisHelper.on('error', error => {
+  console.error(error);
+});
+RedisHelper.on('connect', () => {
+  console.info('Connect to redis cuccess');
+});
